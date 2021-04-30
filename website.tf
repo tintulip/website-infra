b8cd19acab77aa3588a50811a27f8fa89a96f308
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-04-30 11:02:43 +0100

    #9 enable versioning for the S3 buckets
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- website.tf
+++ website.tf
@@ -13,9 +13,16 @@ resource "aws_s3_bucket" "website_root" {
     target_bucket = aws_s3_bucket.website_logs.id
     target_prefix = "logs/"
   }
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "website_logs" {
   bucket = "${var.website_name}-logs"
   acl    = "log-delivery-write"
+
+  versioning {
+    enabled = true
+  }
 }
