41129f916a2604976e8338649f93da3499e2a9a1
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-05-05 15:15:03 +0100

    #13 change the acl for the website bucket to private to stop checkov failing
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- website.tf
+++ website.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "website_root" {
-  #checkov:skip=CKV_AWS_20:The bucket is a public static content host
   #checkov:skip=CKV_AWS_52:Bucket is created by a pipeline
   #checkov:skip=CKV_AWS_144:Not required to have cross region enabled
   bucket = "${var.website_name}-root"
-  acl    = "public-read" #tfsec:ignore:AWS001 
+
+  acl = "private"
   tags = {
     Name        = "CLA Website"
     Environment = "Production"
