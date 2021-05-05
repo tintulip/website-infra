9f0288f32174ced95f311aa36ba3e4aa77bdc3aa
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-05-05 15:28:54 +0100

    #13 limit public access to s3 buckets
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- website.tf
+++ website.tf
@@ -29,6 +29,16 @@ resource "aws_s3_bucket" "website_root" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "website_root" {
+  bucket = aws_s3_bucket.website_root.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
+
 resource "aws_s3_bucket" "website_logs" {
   #checkov:skip=CKV_AWS_52:Bucket is created by a pipeline
   #checkov:skip=CKV_AWS_18:Access logging needs to go into a cross account bucket
@@ -47,4 +57,14 @@ resource "aws_s3_bucket" "website_logs" {
       }
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_logs" {
+  bucket = aws_s3_bucket.website_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+
 }
