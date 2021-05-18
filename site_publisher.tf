c8876659853da9d0d99b91e7fcc4ada4c50b3cd0
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-05-18 11:10:16 +0100

    #36 change the json policy to a policy document due to parsing error.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
    Co-Authored-By: Meissane-tintulip <meissane+tintulip@thoughtworks.com>

--- site_publisher.tf
+++ site_publisher.tf
@@ -126,43 +126,42 @@ POLICY
 }
 
 resource "aws_iam_policy" "log-replication-policy" {
-  name = "log-replication"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:GetReplicationConfiguration",
-        "s3:ListBucket"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.website_logs.arn}"
-      ]
-    },
-    {
-      "Action": [
-        "s3:GetObjectVersionForReplication",
-        "s3:GetObjectVersionAcl",
-        "s3:GetObjectVersionTagging"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.website_logs.arn}/*"
-      ]
-    },
-    {
-      "Action": [
-        "s3:ReplicateObject",
-        "s3:ReplicateDelete",
-        "s3:ReplicateTags"
-      ],
-      "Effect": "Allow",
-      "Resource": ["arn:aws:s3::cla-app-logs/*"]
-    }
-  ]
+  policy = data.aws_iam_policy_document.log-replication-policy.json
+  name = "log-replication-policy"
 }
-POLICY
+
+data "aws_iam_policy_document" "log-replication-policy" {
+  statement {
+    actions = [
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket"
+    ]
+    resources = [
+      aws_s3_bucket.website_logs.arn
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObjectVersionForReplication",
+      "s3:GetObjectVersionAcl",
+      "s3:GetObjectVersionTagging"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.website_logs.arn}/*"
+    ]
+  }
+
+  statement {
+    actions =[
+       "s3:ReplicateObject",
+       "s3:ReplicateDelete",
+       "s3:ReplicateTags"
+    ]
+
+    resources = [
+      "arn:aws:s3::cla-app-logs/*"
+    ]
+  }
 }
