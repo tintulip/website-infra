5c55718dbe9f5be298944b2be9529b34580a83c5
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-05-21 18:08:09 +0100

    #36 Use AWS KMS CMK that lives in log archive account to encrypt bucket
    Co-Authored-By: Yusra Dahir (Tin Tulip) <yusra.dahir+tintulip@thoughtworks.com>
    Co-authored-by: Meissane Chami <meissane.chami+tintulip@thoughtworks.com>

--- log_replication.tf
+++ log_replication.tf
@@ -164,7 +164,8 @@ resource "aws_s3_bucket" "website_logs" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        kms_master_key_id = local.log_rep_kms_key
+        sse_algorithm     = "aws:kms"
       }
     }
   }
