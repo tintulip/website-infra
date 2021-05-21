453d00ebc7079b1c42228312c1d1d22e3058ef9b
Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>
2021-05-21 19:53:45 +0100

    #36 S3 log delivery service does not have access to the KMS key of target bucket

--- log_replication.tf
+++ log_replication.tf
@@ -177,7 +177,7 @@ resource "aws_s3_bucket" "website_logs" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
