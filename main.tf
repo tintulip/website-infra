bd7c59cb5aacb58509515074ad93819e90bc082c
Yusra Dahir (TIN TULIP) <yusra.dahir+tintulip@thoughtworks.com>
2021-06-28 16:13:42 +0100

    #48 change the bucket name to the encrypted with KMS state bucket.
    Co-Authored-By: Rushil Gala-Shah <rgalashah+tintulip@thoughtworks.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket   = "cla-production-state"
+    bucket   = "tfstate-073232250817-production"
     key      = "website-infra/terraform.tfstate"
     region   = "eu-west-2"
     role_arn = "arn:aws:iam::073232250817:role/website-infra"
